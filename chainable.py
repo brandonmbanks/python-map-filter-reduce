39ce77c6c5f2788a7b8f80867d81ae87c46a7883
Brandon Banks <b93banks@gmail.com>
2018-10-17 10:04:14 -0400

    add reduce method

--- chainable.py
+++ chainable.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 class Chainable(object):
     def __init__(self, data):
         self.data = data
@@ -8,4 +11,8 @@ class Chainable(object):
 
     def _filter(self, fn):
         self.data = list(filter(lambda x: fn(x), self.data))
+        return self
+
+    def _reduce(self, fn):
+        self.data = reduce(lambda x, acc: fn(x, acc), self.data)
         return self
--- test_chainable.py
+++ test_chainable.py
@@ -9,6 +9,10 @@ def is_odd(x):
     return x % 2 != 0
 
 
+def multiply(x, y):
+    return x * y
+
+
 def test_map_returns_correct_output():
     chainable = Chainable([1, 2, 3])
     assert [2, 3, 4] == chainable._map(add_1).data
@@ -22,3 +26,8 @@ def test_filter():
 def test_it_can_chain_map_and_filter():
     chainable = Chainable([1, 2, 3])
     assert [3] == chainable._map(add_1)._filter(is_odd).data
+
+
+def test_reduce():
+    chainable = Chainable([1, 2, 3])
+    assert 6 == chainable._reduce(multiply).data
