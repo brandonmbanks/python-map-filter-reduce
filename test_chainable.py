3217d3bd3d1620fe7e80192aa556dd2cbc2e8533
Brandon Banks <b93banks@gmail.com>
2018-11-02 22:13:30 -0400

    add tests for immutability

--- test_chainable.py
+++ test_chainable.py
@@ -46,3 +46,27 @@ def test_reduce():
 def test_reduce_with_function():
     chainable = Chainable([1, 2, 3])
     assert 6 == chainable.reduce(multiply).data
+
+
+def test_map_should_not_mutate_the_instance():
+    chainable = Chainable([1, 2, 3])
+    chainable.map(lambda x: x + 1)
+    assert [1, 2, 3] == chainable.data
+    chainable = chainable.map(lambda x: x + 1)
+    assert [2, 3, 4] == chainable.data
+
+
+def test_filter_should_not_mutate_the_instance():
+    chainable = Chainable([1, 2, 3])
+    chainable.filter(lambda x: x % 2 != 0)
+    assert [1, 2, 3] == chainable.data
+    chainable = chainable.filter(lambda x: x % 2 != 0)
+    assert [1, 3] == chainable.data
+
+
+def test_reduce_should_not_mutate_the_instance():
+    chainable = Chainable([1, 2, 3])
+    chainable.reduce(lambda x, acc: x + acc)
+    assert [1, 2, 3] == chainable.data
+    chainable = chainable.reduce(lambda x, acc: x + acc)
+    assert 6 == chainable.data
